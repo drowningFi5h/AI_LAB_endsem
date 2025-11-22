020ef5b5d5868a34549022d59eb7d4bf9da20b94
Tausif Ansari <tausifmdansari1@gmail.com>
2025-11-23 03:21:45 +0530

    Lab5 code

--- sem5/sem-5/AI-CS307/d-seprationGraph/inlab.R
+++
@@ -1,6 +0,0 @@
-library(bnlearn)
-
-course.grades<-read.table("2020_course_grades.txt",head=TRUE)
-course.grades.net<-hc(course.grades,"k2")
-course.grades.net.fit<-bn.fit(course.grades.net, course.grades)
-plot(course.grades.net)
